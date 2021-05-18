0805aba74880f85af57fada67580eed83dc66e68
Andrés Felipe <andres.cardozo96@gmail.com>
2021-05-18 15:12:17 -0500

    *Se crea accion de login

--- src/index.js
+++ src/index.js
@@ -17,8 +17,6 @@ import AuthLayout from "layouts/Auth.js";
 //redux
 import {Provider} from "react-redux";
 import generateStore from "./redux/store";
-import {ApolloProvider} from "react-apollo";
-import {ApolloClient} from "apollo-boost";
 
 let WithThemeProvider = () => <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
@@ -33,15 +31,10 @@ let WithThemeProvider = () => <ThemeProvider theme={theme}>
 </ThemeProvider>;
 
 let store = generateStore();
-let client = new ApolloClient({
-    uri: "http://localhost:8000/graphql"
-});
 
 let WithProvider = () => <Provider store={store}><WithThemeProvider/></Provider>;
 
-let WithApollo = () => <ApolloProvider client={client}><WithProvider/></ApolloProvider>
-
 ReactDOM.render(
-    <WithApollo/>,
+    <WithProvider/>,
     document.querySelector("#root")
 );
--- src/redux/UserDucks.js
+++ src/redux/UserDucks.js
@@ -1,22 +1,62 @@
+import ApolloClient, {gql} from "apollo-boost";
+
 const initialData = {
+    fetching: false,
+    accessToken: null,
     authUser: {},
-    users:[]
+    users: []
 }
 
+const apolloClient = new ApolloClient({
+    uri: "http://155.138.201.98:8000/graphql"
+});
+
 const LOGIN_USER = "LOGIN_USER";
+const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
+const LOGIN_USER_ERROR = "LOGIN_USER_ERROR";
 
 export default function userReducer(state = initialData, action) {
     switch (action.type) {
         case LOGIN_USER:
-            break;
-
+            return {...state, fetching: true}
+        case LOGIN_USER_SUCCESS:
+            return {...state, fetching: false, accessToken: action.payload}
+        case LOGIN_USER_ERROR:
+            return {...state, fetching: false, error: action.payload}
         default:
             return state;
     }
 }
 
-export const loginAction = () => (dispatch, getState) => {
+export const loginAction = (email, password) => (dispatch, getState) => {
     dispatch({
         type: LOGIN_USER
+    });
+
+    let mutation = gql`
+    mutation ($email: String!, $password: String!) {
+  login(input: { username: $email, password: $password }) {
+    access_token
+  }
+  }`;
+
+    apolloClient.mutate({
+        mutation,
+        variables: {email, password}
+    })
+        .then(({data}) => {
+            let accessToken = data.login.access_token;
+            localStorage.setItem('access_token',accessToken)
+
+            dispatch({
+                    type: LOGIN_USER_SUCCESS,
+                    payload: accessToken
+                });
+            }
+        ).catch(error => {
+        dispatch({
+            type: LOGIN_USER_ERROR,
+            payload: error
+        });
     });
 }
